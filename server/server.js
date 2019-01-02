8f662843c2aac3d96e353c7ba7076c8075ddcc96
benkissi <benkissi111@gmail.com>
2019-01-02 20:04:23 +0000

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ var {ObjectID} = require('mongodb');
 var {authenticate} = require('./middleware/authenticate');
 
 
+
 var app = express();
 const port = process.env.PORT || 3000
 
@@ -105,6 +106,18 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user)                  
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
+})
 
 
 app.listen(port, ()=> {
